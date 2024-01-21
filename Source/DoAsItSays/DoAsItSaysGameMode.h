9863a1920628d2d7460e22c86428a4de864cb21f
Gabriela Matias <jasbrela@gmail.com>
2024-01-21 00:09:34 +0000

    create project from template

---
+++ Source/DoAsItSays/DoAsItSaysGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DoAsItSaysGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADoAsItSaysGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADoAsItSaysGameMode();
+};
+
+
+
